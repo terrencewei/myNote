02ff9b786812c21dce1de69e8e093e7160e5eecc
terrencewei <terrencewei@aaxischina.com>
2017-05-02 17:59:50 +0800

    create interface in js

--- src/main/resources/static/js/utils/editorUtils.js
+++ src/main/resources/static/js/utils/editorUtils.js
@@ -1,6 +1,12 @@
 /** ---------------------------------------------------------------------------------- */
 /** editorUtils
  /** ---------------------------------------------------------------------------------- */
+// enum
+if (typeof ObjType == "undefined") {
+	var ObjType = {};
+	ObjType.OSS = 1;
+	ObjType.Local = 2;
+}
 var editorUtils = {
 	config: {
 		containerId: "myEditormd",
@@ -20,33 +26,45 @@ var editorUtils = {
 			},
 			list: function (successFn) {
 				// to be implements by other module
+			},
+			putLocal: function (successFn, objKey, objData) {
+				// to be implements by other module
+			},
+			getLocal: function (successFn, objKey) {
+				// to be implements by other module
+			},
+			listLocal: function (successFn) {
+				// to be implements by other module
 			}
 		}
 	},
-	oss: {
-		save: function (thisEditormd) {
+	obj: {
+		put: function (thisEditormd, objType) {
 			maskUtils.show(true);
 
 			if (editorUtils.shared.data.currentObjKey != null) {
 				// save to OSS
-				editorUtils.shared.fn.put(function (ossOutput) {
+				var successFn = function (ossOutput) {
 					maskUtils.show(false);
-					msgUtils.success("\" " + ossOutput.objects[0].key + " \" 已保存到云端 (" + ((ossOutput.objects[0].size) / 1024).toFixed(2) + "KB)");
-				}, editorUtils.shared.data.currentObjKey, thisEditormd.getMarkdown());
+					msgUtils.success({
+						"oss": "\" " + ossOutput.objects[0].key + " \" 已保存到云端 (" + ((ossOutput.objects[0].size) / 1024).toFixed(2) + "KB)",
+						"local": "\" " + ossOutput.objects[0].key + " \" 已保存到本地 (" + ((ossOutput.objects[0].size) / 1024).toFixed(2) + "KB)",
+					}[objType]);
+				};
+				var putInvoker = {
+					"oss": editorUtils.shared.fn.put(successFn, editorUtils.shared.data.currentObjKey, thisEditormd.getMarkdown()),
+					"local": editorUtils.shared.fn.putLocal(successFn, editorUtils.shared.data.currentObjKey, thisEditormd.getMarkdown()),
+				}[objType];
 			} else {
 				msgUtils.warn("你还没有选择任何文件");
 				maskUtils.show(false);
 			}
 		},
-		getAll: function (thisEditormd) {
-
+		list: function (thisEditormd, objType) {
 			maskUtils.show(true);
-
 			// call service to get all files from OSS
-			editorUtils.shared.fn.list(function (ossOutput) {
-
+			var listSuccessFn = function (ossOutput) {
 				maskUtils.show(false);
-
 				var popupContent = $('<div class="list-group"></div>')
 					.append('<a class="list-group-item active">Click a file to edit</a>');
 				// each line content
@@ -55,21 +73,25 @@ var editorUtils = {
 						$('<a class="list-group-item">' + object.key + '</a>')
 							.on("click", function () {
 								// call service to get single file content from OSS
-								editorUtils.shared.fn.get(function (ossOutput) {
-
+								var getSuccessFn = function (ossOutput) {
 									popupUtils.close(thisEditormd);
-
 									editorUtils.shared.data.currentObjKey = object.key;
-
 									thisEditormd.cm.setValue(ossOutput.objects[0].content);
-
-								}, object.key);
+								};
+								var getInvoker = {
+									"oss": editorUtils.shared.fn.get(getSuccessFn, object.key),
+									"local": editorUtils.shared.fn.getLocal(getSuccessFn, object.key),
+								}[objType];
 							})
 					);
 				});
 				popupUtils.update(popupContent, thisEditormd);
 				popupUtils.show(thisEditormd);
-			});
+			};
+			var listInvoker = {
+				"oss": editorUtils.shared.fn.list(listSuccessFn),
+				"local": editorUtils.shared.fn.listLocal(listSuccessFn),
+			}[objType];
 		}
 	},
 	init: function () {
@@ -85,31 +107,51 @@ var editorUtils = {
 			tocTitle: "目录",
 			toolbar: true,
 			toolbarIcons: function () {
-				return ["ossSave", "ossGetAll", "|", "gotoDevExamples"];
+				return ["ossDownload", "ossUpload", "|", "syncCloud2Local", "syncLocal2Cloud", "|", "openLocal", "saveLocal", "|", "gotoDevExamples"];
 			},
 			toolbarIconsClass: {
 				// FontAawsome class
+				ossDownload: "fa-cloud-download",
+				ossUpload: "fa-cloud-upload",
+				syncCloud2Local: "fa-download",
+				syncLocal2Cloud: "fa-upload",
+				openLocal: "fa-folder-open-o",
+				saveLocal: "fa-floppy-o",
 				gotoDevExamples: "fa-hand-o-right",
-				ossSave: "fa-cloud-upload",
-				ossGetAll: "fa-cloud-download"
 			},
 			lang: {
 				toolbar: {
+					ossDownload: "打开云端文件",
+					ossUpload: "保存到云端",
+					syncCloud2Local: "下载云端覆盖本地",
+					syncLocal2Cloud: "上传本地覆盖云端",
+					openLocal: "打开本地文件",
+					saveLocal: "保存到本地",
 					gotoDevExamples: "查看Editor.md开发者示例",
-					ossSave: "保存到云端",
-					ossGetAll: "从云端获取所有"
 				}
 			},
 			toolbarHandlers: {
+				ossDownload: function () {
+					editorUtils.obj.list(this, "oss");
+				},
+				ossUpload: function () {
+					editorUtils.obj.put(this, "oss");
+				},
+				syncCloud2Local: function () {
+					alert("TODO");
+				},
+				syncLocal2Cloud: function () {
+					alert("TODO");
+				},
+				openLocal: function () {
+					alert("TODO");
+				},
+				saveLocal: function () {
+					alert("TODO");
+				},
 				gotoDevExamples: function (cm, icon, cursor, selection) {
 					window.open(editorUtils.config.devExamplePath);
 				},
-				ossSave: function () {
-					editorUtils.oss.save(this);
-				},
-				ossGetAll: function () {
-					editorUtils.oss.getAll(this);
-				}
 			},
 			onload: function () {
 				var _this = this;
@@ -126,12 +168,12 @@ var editorUtils = {
 				});
 
 				_this.addKeyMap({
-					"Ctrl-S": function (cm) {
-						editorUtils.oss.save(_this);
-					},
 					"Ctrl-O": function (cm) {
-						editorUtils.oss.getAll(_this);
-					}
+						editorUtils.obj.list(_this, "oss");
+					},
+					"Ctrl-S": function (cm) {
+						editorUtils.obj.put(_this, "oss");
+					},
 				});
 			}
 		});
